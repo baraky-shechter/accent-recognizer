c219bc194012bf0eedd4567faec4dcc986e54396
Barak Shechter <barakshechter@Baraks-MacBook-Pro-2.local>
2020-02-25 14:28:42 -0500

    added logic

--- dataset.py
+++ dataset.py
@@ -4,7 +4,15 @@ import torch
 import pandas as pd
 import numpy as np
 
-dataset_pathname = './dataset/'
-dataset_csv_pathname = dataset_pathname + 'speakers_all.csv'
-dataset_recordings_folder_pathname = dataset_pathname + 'recordings/'
+print("worked")
+
+class Dataset:
+
+    def __init__(self):
+        dataset_pathname = './dataset/'
+        dataset_csv_pathname = dataset_pathname + 'speakers_all.csv'
+        dataset_recordings_folder_pathname = dataset_pathname + 'recordings/'
+
+        print(dataset_pathname + "\n" + dataset_csv_pathname + "\n" + dataset_recordings_folder_pathname)
+
 
--- main.py
+++ main.py
@@ -5,16 +5,20 @@ import torchvision
 import torchvision.transforms as transforms
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+import dataset as d
+
+
 
 def setupDevice():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print("Using device: ", device)
 
 def processDataset():
-    raise NotImplementedError
+    dataset = d.Dataset()
 
 def main():
     setupDevice()
+    processDataset()
 
 if __name__ == "__main__":
     main()
